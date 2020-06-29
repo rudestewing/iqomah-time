04341d5db2e83bde1e4e680837977dde59c0adf5
Rudi Setiawan <rudestewing@gmail.com>
2020-06-29 09:55:21 +0700

    home slider component

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ Vue.component('app-layout-default', require('./layouts/Default.vue').default);
 Vue.component('app-iqomah-countdown', require('./components/IqomahCountdown.vue').default);
 Vue.component('app-reminder-upcoming-time', require('./components/ReminderUpcomingTime.vue').default);
 Vue.component('app-time-item', require('./components/TimeItem.vue').default);
+Vue.component('app-home-slider', require('./components/HomeSlider.vue').default);
 
 const app = new Vue({
 	router: router,
---
+++ resources/js/components/HomeSlider.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="home-slider">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+
+}
+</script>
